6cfae6ecdfd6700262568d010415fbe691bfa9f8
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-09-03 14:54:29 +0000

    Install React Router library and attempt link wire up for Home,  login and sign up - not currently  working as expected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Navbar, Container, Nav, Row, NavLink } from 'react-bootstrap';
+import { Navbar, Container, Nav, } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return ( 
@@ -9,27 +10,27 @@ const NavBar = () => {
       <Container>
         
         <Nav className="text-right">
-          <Nav.Link>
+          <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
             <Navbar.Brand className='logo'><img src={logo} alt="logo" height="45" />
               </Navbar.Brand>
               The Winding Path    
-              </Nav.Link>
+              </NavLink>
         </Nav>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-            <i className="fa-solid fa-house-chimney"></i>
-              Home     
-            </Nav.Link>
-            <Nav.Link>
-            <i className="fa-solid fa-right-to-bracket"></i>
-              Log in
-            </Nav.Link>
-            <Nav.Link>
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+              <i className="fa-solid fa-house-chimney"></i>
+                Home     
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
+              <i className="fa-solid fa-right-to-bracket"></i>
+                Log in
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
             <i className="fa-solid fa-pen-to-square"></i>
               Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
