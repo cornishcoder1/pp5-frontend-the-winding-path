50b1b45987a6607f2c774b66cff1c38b6e6205aa
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-09-03 13:47:44 +0000

    Style Logged Out NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/logo.png'
+import { Navbar, Container, Nav, Row, NavLink } from 'react-bootstrap';
+import logo from '../assets/logo.png';
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return ( 
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand><img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        
+        <Nav className="text-right">
+          <Nav.Link>
+            <Navbar.Brand className='logo'><img src={logo} alt="logo" height="45" />
+              </Navbar.Brand>
+              The Winding Path    
+              </Nav.Link>
+        </Nav>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
