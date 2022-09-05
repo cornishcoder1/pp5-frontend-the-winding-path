ed77c4a327d6e337a6c8c44499065159ab63130b
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-09-05 14:34:36 +0000

    Add Form to SignUpForm.js, modify and style inputs and button

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -15,12 +15,40 @@ const SignUpForm = () => {
       <Col>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">username</Form.Label>
+              <Form.Control
+              type="text"
+              placeholder="Username"
+              name="username" />
+            </Form.Group>
 
-            {/* add your form here */}
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password1" />
+            </Form.Group>
 
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              name="password2" />
+            </Form.Group>
+
+            <Button
+            className={btnStyles.Button}
+            type="submit">
+              Sign up
+            </Button>
+            </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
+          <Link className={styles.LinkSignin} to="/signin">
             Already have an account? <span>Sign in</span>
           </Link>
         </Container>
