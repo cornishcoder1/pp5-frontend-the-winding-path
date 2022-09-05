a6e67578ac5d18fa3d5117ee247e6d7898a20e8f
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-09-05 16:21:29 +0000

    Fix spacing errors in LogInForm.js (caused latest build to fail in Heroku

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -45,64 +45,64 @@ function LogInForm() {
 
 
 
-    return (    
-        <Row className={styles.Row}>
-          <Col>
-            <Container className={`${appStyles.Content} p-4 `}>
-              <h1 className={styles.Header}>Log in</h1>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                  <Form.Label className="d-none">username</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Username"
-                    name="username"
-                    value={username}
-                    onChange={handleChange}
-                  />
-                </Form.Group>
-                {errors.username?.map((message, idx) => (
-                  <Alert key={idx} variant="warning">
-                    {message}
-                  </Alert>
-                ))}
-    
-                <Form.Group controlId="password">
-                  <Form.Label className="d-none">password</Form.Label>
-                  <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  value={password}
+  return (    
+      <Row className={styles.Row}>
+        <Col>
+          <Container className={`${appStyles.Content} p-4 `}>
+            <h1 className={styles.Header}>Log in</h1>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="username">
+                <Form.Label className="d-none">username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
                   onChange={handleChange}
-                  />
-                </Form.Group>
-                {errors.password?.map((message, idx) => (
-                  <Alert key={idx} variant="warning">
+                />
+              </Form.Group>
+              {errors.username?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                  {message}
+                </Alert>
+              ))}
+    
+              <Form.Group controlId="password">
+                <Form.Label className="d-none">password</Form.Label>
+                <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                  {message}
+                </Alert>
+              ))}
+    
+              <Button
+              className={btnStyles.Button}
+              type="submit">
+                Login
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                  <Alert key={idx} variant="warning" className="mt-3">
                     {message}
                   </Alert>
-                ))}
+              ))}
+              </Form>
+          </Container>
+          <Container className={`mt-3 ${appStyles.Content}`}>
+            <Link className={styles.LinkSignin} to="/signup">
+              Do not have account? <span>Sign up</span>
+            </Link>
+          </Container>
+        </Col>
+      </Row>
+    );
+}
     
-                <Button
-                className={btnStyles.Button}
-                type="submit">
-                  Login
-                </Button>
-                {errors.non_field_errors?.map((message, idx) => (
-                    <Alert key={idx} variant="warning" className="mt-3">
-                      {message}
-                    </Alert>
-                ))}
-                </Form>
-            </Container>
-            <Container className={`mt-3 ${appStyles.Content}`}>
-              <Link className={styles.LinkSignin} to="/signup">
-                Don't have an account? <span>Sign up</span>
-              </Link>
-            </Container>
-          </Col>
-        </Row>
-      );
-    };
-    
-    export default LogInForm;
+export default LogInForm;
