4e3f06e3e95cd66982be76586235ab193fe218bb
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-09-06 07:48:02 +0000

    Add eslint-disable to CurrentUserContext.js to allow Heroku build to deploy

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
