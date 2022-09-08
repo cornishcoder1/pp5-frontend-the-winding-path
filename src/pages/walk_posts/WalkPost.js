b50f0a47e706271bc546df00ecf86a5b48cf0c69
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-09-08 09:57:09 +0000

    Add additional walk posts fields to return statement so that they display on the WalkPost component

--- src/pages/walk_posts/WalkPost.js
+++ src/pages/walk_posts/WalkPost.js
@@ -68,68 +68,66 @@ const WalkPost = (props) => {
 
 
     return <Card className={styles.WalkPost}>
-    <Card.Body>
-        <Media className="align-items-center justify-content-between">
-            <Link to={`/profiles/${profile_id}`}>
-                <Avatar src={profile_image} height={55} />
-                {owner}
-            </Link>
-            <div className="d-flex align-items-center">
-                <span>
-                    {is_owner ? (
-                    <OverlayTrigger
-                    placement="top"
-                    overlay={<Tooltip>You cannot save your own post!</Tooltip>}
-                    >
-                    <i className="far fa-bookmark" />
-                    </OverlayTrigger>
-                    ) : save_id ? (
-                    <span onClick={handleUnsave}>
-                    <i className={`fas fa-bookmark ${styles.Bookmark}`} />
+        <Card.Body>
+            <Media className="align-items-center justify-content-between">
+                <Link to={`/profiles/${profile_id}`}>
+                    <Avatar src={profile_image} height={55} />
+                    {owner}
+                </Link>
+                <div className="d-flex align-items-center">
+                    <span>
+                        {is_owner ? (
+                        <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>You cannot save your own post!</Tooltip>}
+                        >
+                        <i className="far fa-bookmark" />
+                        </OverlayTrigger>
+                        ) : save_id ? (
+                        <span onClick={handleUnsave}>
+                        <i className={`fas fa-bookmark ${styles.Bookmark}`} />
+                        </span>
+                        ) : currentUser ? (
+                        <span onClick={handleSave}>
+                        <i className={`far fa-bookmark ${styles.BookmarkOutline}`} />
+                        </span>
+                        ) : (
+                        <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>Log in to save walk posts!</Tooltip>}
+                        >
+                        <i className="far fa-bookmark" />
+                        </OverlayTrigger>
+                        )}
                     </span>
-                    ) : currentUser ? (
-                    <span onClick={handleSave}>
-                    <i className={`far fa-bookmark ${styles.BookmarkOutline}`} />
-                    </span>
-                    ) : (
-                    <OverlayTrigger
-                    placement="top"
-                    overlay={<Tooltip>Log in to save walk posts!</Tooltip>}
-                    >
-                    <i className="far fa-bookmark" />
-                    </OverlayTrigger>
-                    )}
-                </span>
+                    {is_owner && walkPostPage && "..."}
+                </div>
+            </Media>
+        </Card.Body>
+        <Link to={`/walk-posts/${id}`}>
+            <Card.Img src={image} alt={title} />
+        </Link>
+        <Card.Body>
+            {updated_on && <Card.Title className="text-centre">{updated_on}</Card.Title>} 
+            {title && <Card.Title className="text-centre">{title}</Card.Title>}
+            {headline && <Card.Text className={styles.Headline}>{headline}</Card.Text>} 
+            <p>Environment: {environment}</p>
+            <p>WC en-route?: {wc}</p>
+            <p>Dog-friendly?: {dog_friendly}</p>
+            <p>Difficuly: {difficulty}</p>
+            <p>Length: {length}</p>
+            <p>Duration: {duration}</p>
+            {content && <Card.Text>{content}</Card.Text>} 
 
-
-
-
-
-                {is_owner && walkPostPage && "..."}
+            <div className={styles.PostBar}>
+                <Link to={`/walk-posts/${id}`}>
+                    <i className="far fa-comments" />
+                </Link>
+                {comments_count}               
             </div>
-        </Media>
-    </Card.Body>
-
-
-
-
-    {/* <Link to={`/gallery-posts/${id}`}>
-        <Card.Img src={image} alt={title} />
-    </Link>
-    <Card.Body>
-        {updated_on && <Card.Title className="text-centre">{updated_on}</Card.Title>} 
-        {title && <Card.Title className="text-centre">{title}</Card.Title>}
-        {headline && <Card.Text>{headline}</Card.Text>}       
-        <div className={styles.PostBar}>
-            
-            <Link to={`/gallery-posts/${id}`}>
-                <i className="far fa-comments" />
-            </Link>
-            {comments_count}
-        </div>
-    </Card.Body> */}
-</Card>
-}
+        </Card.Body>
+    </Card>
+};
     
 
 export default WalkPost
