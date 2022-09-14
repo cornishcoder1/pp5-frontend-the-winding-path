716d52c2ef1543f8411bba0cb3d558d74331ae20
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-09-14 09:35:47 +0000

    comment out footer whilst re-styling add walk post page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import ProfilePage from './pages/profiles/ProfilePage';
 import UsernameForm from "./pages/profiles/UserNameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import ContactForm from './pages/contact/ContactForm';
 import Confirmation from './pages/contact/Confirmation';
 
@@ -104,7 +104,7 @@ function App() {
 					<Route render={() => <p>Page not found!</p>} />
 						
 				</Switch>
-			<Footer />	
+			{/* <Footer />	 */}
 			</Container>
 		</div>
 	);
